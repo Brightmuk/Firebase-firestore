97c609d07ef4de1043fecce4357cdddf4ad0e2c3
Bright <bright@RUTHs-MacBook-Air.local>
2020-06-22 15:29:53 +0300

    Create separate file for firbase config

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCLYvA89xkj-hmrdiKFWteeeg-2ZQrRF1Q",
+    authDomain: "firestore-web-83e7c.firebaseapp.com",
+    databaseURL: "https://firestore-web-83e7c.firebaseio.com",
+    projectId: "firestore-web-83e7c",
+    storageBucket: "firestore-web-83e7c.appspot.com",
+    messagingSenderId: "149925039142",
+    appId: "1:149925039142:web:6dfb3dc97c9b360fe4eae7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db= firebase.firestore();
