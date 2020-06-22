98dda287b5391b266b013e9032bc4a9fd051b602
Bright <bright@RUTHs-MacBook-Air.local>
2020-06-22 14:23:40 +0300

    Add bootstrap cdn for modal

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ function renderCafe(doc){
         db.collection("cafe's").doc(id).delete();
     })
 }
-//get
-db.collection("cafe's").orderBy('name').get().then((snapshot)=>{
-snapshot.docs.forEach(doc => {
-    renderCafe(doc);
-});
-})
+// //get
+// db.collection("cafe's").orderBy('name').get().then((snapshot)=>{
+// snapshot.docs.forEach(doc => {
+//     renderCafe(doc);
+// });
+// })
 //save
 addCafeform.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -42,3 +42,16 @@ addCafeform.addEventListener('submit', (e)=>{
     addCafeform.name.value='';
     addCafeform.city.value='';
 })
+
+db.collection("cafe's").orderBy('name').onSnapshot(snapshot=>{
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+       if (change.type=='added'){
+           renderCafe(change.doc);
+       }else if(change.type=='removed'){
+           let li = cafeList.querySelector('[data-id='+change.doc.id+']');
+           cafeList.removeChild(li);
+       }
+
+    });
+});
